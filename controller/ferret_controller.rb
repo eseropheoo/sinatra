dde4499fdabeeaf9c85b1f35e648f2c61c9e40c3
Eserophe Ovie-Okoro <eseropheoo@globalsparta.co>
2017-06-04 13:28:32 +0100

    complete controller and added forms and input

---
+++ controller/ferret_controller.rb
@@ -0,0 +1,45 @@
+class FerretsController < Sinatra::Base
+ 	configure :development do
+		register Sinatra::Reloader
+		set :public_folder, 'public'
+	end
+
+		#INDEX
+
+		# get '/ferrets' do
+		# 	'<h1>Rabid Ferrets<h1>'
+		# end
+
+		#NEW
+		get '/ferrets/new' do
+			'NEW Ferrets'
+		end
+		#SHOW
+		get '/ferrets/:id' do
+			id = params[:id]
+		end
+		#CREATE
+		post '/ferrets' do
+			"BREED MORE FERRETS"
+		end
+		#UPDATE 
+		patch '/ferrets/:id' do
+			id = params[:id]
+		end
+		#DELETE
+		delete '/ferrets/:id' do
+			id = params[:id]
+		end
+		#EDIT
+		get '/ferrets/:id/edit' do
+			"EDIT #{params[:id]}"
+		end
+
+		#SEND HTML
+		get '/ferrets' do
+			send_file 'public/index.html'
+
+		end
+
+
+end
